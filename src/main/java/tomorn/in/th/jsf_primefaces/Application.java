67baeda13fed44db640a17fdc31c39623e85f6bb
tomornw <dekcom007@gmail.com>
2020-09-21 15:37:01 +0700

    add method

--- src/main/java/tomorn/in/th/jsf_primefaces/Application.java
+++ src/main/java/tomorn/in/th/jsf_primefaces/Application.java
@@ -1,8 +1,11 @@
 package tomorn.in.th.jsf_primefaces;
 
 import javax.faces.webapp.FacesServlet;
+
+import org.eclipse.jdt.internal.compiler.ast.Initializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
@@ -13,16 +16,21 @@ import org.springframework.context.annotation.ComponentScan;
  * @author tomornw
  */
 @EnableAutoConfiguration
-@ComponentScan({"tomorn.in.th.jsf_primefaces"})
+@ComponentScan({ "tomorn.in.th.jsf_primefaces" })
 public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        return application.sources(Application.class, Initializer.class);
     }
 
     @Bean
     public ServletRegistrationBean servletRegistrationBean() {
-        FacesServlet servlet = new FacesServlet();
+        final FacesServlet servlet = new FacesServlet();
         return new ServletRegistrationBean(servlet, "*.jsf");
     }
 }
