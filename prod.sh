8851054c2bb7de77f6edc15600c26ce84863d12e
Tomorn wongtibet <dekcom007@gmail.com>
2020-10-05 11:38:38 +0700

    Update prod.sh

--- prod.sh
+++ prod.sh
@@ -7,12 +7,12 @@ echo "spring"
 echo "stop container and remove image"
 docker container rm -f spring
 docker image rm -f spring
-echo "Go to Folder Script File And Dockerfile"
-cd ~/mitrphol/spring-warfile/
+#echo "Go to Folder Script File And Dockerfile"
+#cd ~/mitrphol/spring-warfile/
 echo "package"
-mvn clean install package
+mvn clean package
 echo "build image from docker file"
-mvn spring-boot:build-image -Dspring-boot.build-image.imageName=spring
+docker build -t spring .
 echo "run image to container"
-docker run --restart=always -e TZ="Asia/Bangkok" --name spring -p 9999:9999 -d spring
+docker run --restart=always --name spring -p 9999:9999 -d spring
 echo "finish step"
